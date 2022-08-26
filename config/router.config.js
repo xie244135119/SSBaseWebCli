dd1e6e5edac9ff4c1368378da1527a5e2c6c696a
Murphy.xie <murphy.xie@yunzhihui.com>
2022-08-26 14:05:54 +0800

    Modify pnpm init

--- config/router.config.js
+++ config/router.config.js
@@ -1,24 +1,5 @@
 export default [
   {
-    path: '/test',
-    component: './pages/test',
-    name: '测试网页',
-    children: [
-      {
-        path: './child',
-        name: '嵌套路由',
-        component: './pages/child',
-        children: [
-          {
-            path: './second',
-            name: '嵌套子路由',
-            component: './pages/secondchild'
-          }
-        ]
-      }
-    ]
-  },
-  {
     path: '/directory',
     component: './pages/Help/directory'
   },
