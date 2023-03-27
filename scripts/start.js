8003f27b5573a89217133e18fe1424cb14e6a5e7
Murphy.xie <murphy.xie@yunzhihui.com>
2023-03-27 10:05:44 +0800

    Update project demo

--- scripts/start.js
+++ scripts/start.js
@@ -1,137 +1,45 @@
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
 
-const fs = require('fs');
-const gracefulFs = require('graceful-fs');
-gracefulFs.gracefulify(fs);
-const webpack = require('webpack');
-const WebDevServer = require('webpack-dev-server');
-const loadsh = require('lodash');
-const chalk = require('chalk');
-const spawn = require('cross-spawn');
-const dayjs = require('dayjs');
-const webpackbar = require('webpackbar');
-const paths = require('../webpack/paths');
+import * as vite from 'vite';
+import lodash from 'lodash';
+import chalk from 'chalk';
+import spawn from 'cross-spawn';
+import dayjs from 'dayjs';
+// import paths from '../vite/paths';
+import viteConfig from '../vite.config';
+
+// const fs = require('fs');
+// const gracefulFs = require('graceful-fs');
+// gracefulFs.gracefulify(fs);
+// const loadsh = require('lodash');
+// const chalk = require('chalk');
+// const spawn = require('cross-spawn');
+// const dayjs = require('dayjs');
+// const paths = require('../vite/paths');
+// const devServerUtil = require('../vite/devServerUtil');
+// const viteConfig = require('../vite/vite.config');
+// const { devServer } = require('../webpack/webpack.config.dev');
 
 const LOG_PREFIX = require(paths.packageJsonPath).cliType;
 
-const toJsonOptionsObject2 = {
-  // all log
-  all: false,
-  assets: false,
-  /** Sort assets by a field */
-  assetsSort: 'size',
-  /** Add built at time information */
-  builtAt: true,
-  // module assets
-  moduleAssets: false,
-  // show how many assets
-  assetsSpace: 6,
-  // module how many space
-  modulesSpace: 10,
-  // how many chunk modules
-  chunkModulesSpace: 10,
-  // 在模块内部显示嵌套的其他模块
-  nestedModules: true,
+// create dev server
+vite
+  .createServer(viteConfig)
+  .then((aDevServer) => {
+    console.log(' vite dev server ');
 
-  /** Add information about cached (not built) modules */
-  cached: false,
-  // 是否缓存非内置模块的
-  cachedModules: false,
-  // 是否添加缓存的其他模块信息
-  runtimeModules: false,
-  // 是否显示该 chunk 依赖的其他模块
-  dependentModules: false,
-
-  // 按照 asset 与 chunk 的关系进行分组
-  groupAssetsByChunk: true,
-  //
-  groupAssetsByEmitStatus: false,
-
-  /** Show cached assets (setting this to `false` only shows emitted files) */
-  cachedAssets: true,
-  /** Add children information */
-  children: true,
-
-  /** Add chunk information (setting this to `false` allows for a less verbose output) */
-  chunks: false,
-  /** Add information about the `namedChunkGroups` */
-  chunkGroups: false,
-  /** Add built modules information to chunk information */
-  chunkModules: false,
-  /** Add the origins of chunks and chunk merging info */
-  chunkOrigins: false,
-
-  /** Sort the chunks by a field */
-  // chunksSort: string | undefined;
-  /** Context directory for request shortening */
-  // context: string | undefined;
-  /** Display the distance from the entry point for each module */
-  depth: false,
-  /** Display the entry points with the corresponding bundles */
-  entrypoints: false,
-  /** Add --env information */
-  env: true,
-  /** Add errors */
-  errors: true,
-  errorStack: true,
-  errorsCount: true,
-  /** Add details to errors (like resolving log) */
-  errorDetails: 'auto',
-  /** Add the hash of the compilation */
-  hash: true,
-
-  //
-  logging: 'error',
-  //
-  loggingTrace: true,
-  /** Add built modules information */
-  modules: false,
-  /** Sort the modules by a field */
-  // modulesSort: string | undefined;
-  /** Show dependencies and origin of warnings/errors */
-  moduleTrace: true,
-
-  //
-  optimizationBailout: true,
-  //
-  outputPath: false,
-  /** Show performance hint when file size exceeds `performance.maxAssetSize` */
-  performance: true,
-  // 统计人员信息行为分析
-  preset: 'minimal',
-
-  //
-  /** Show the exports of the modules */
-  providedExports: false,
-  //
-  warningsCount: true,
-  // 模块被引用的原因
-  reasons: true,
-
-  // 源码
-  source: true,
-  // 时间
-  timings: true,
-  /** Add webpack version information */
-  version: true,
-
-  //
-  ids: true,
-  /** Show which exports of a module are used */
-  usedExports: false,
-
-  /** Add public path information */
-  publicPath: false,
-  /** Add information about the reasons why modules are included */
-  reasons: true,
-
-  /** Add warnings */
-  warnings: true
-
-  /** Filter warnings to be shown */
-  // warningsFilter: null;
-};
+    // console.log(' vite 数据信息 import.meta ', import.meta);
+    // listen
+    // return devServerUtil.getActivePort().then((newPort) => {
+    //   console.log(' 服务器启动 端口号 ', newPort);
+    //   //
+    // return aDevServer.listen(newPort, false);
+    // });
+  })
+  .then((aDevServer) => {
+    console.log(' 服务器启动 ');
+  });
 
 const toJsonOptionsObject = {
   preset: 'verbose'
@@ -264,37 +172,33 @@ const closeCombileAndServer = (callBack = () => {}) => {
   });
 };
 
-runCombileAndServer().then((res) => {
-  rootCompile = res[0];
-  runingServer = res[1];
-});
+// const configfileWatchHandler = (event, filename) => {
+//   // console.log(chalk.yellow(`${filename} exist ${event}`));
+//   if (event === 'change') {
+//     // spawn.sync('node', [paths.scriptPath + '/start.js'], {
+//     //   stdio: 'inherit'
+//     // });
+//     closeCombileAndServer(() => {
+//       runCombileAndServer().then((res) => {
+//         rootCompile = res[0];
+//         runingServer = res[1];
+//       });
+//     });
+//   }
+// };
 
-const configfileWatchHandler = (event, filename) => {
-  // console.log(chalk.yellow(`${filename} exist ${event}`));
-  if (event === 'change') {
-    // spawn.sync('node', [paths.scriptPath + '/start.js'], {
-    //   stdio: 'inherit'
-    // });
-    closeCombileAndServer(() => {
-      runCombileAndServer().then((res) => {
-        rootCompile = res[0];
-        runingServer = res[1];
-      });
-    });
-  }
-};
 // const fswatcher1 = fs.watch(paths.scriptPath, { recursive: true }, configfileWatchHandler);
-const fswatcher2 = fs.watch(paths.webpackPath, { recursive: true }, (event, filename) => {
-  delete require.cache[require.resolve('../webpack/webpack.config.dev')];
-  delete require.cache[require.resolve('../webpack/webpack.config.base')];
-  configfileWatchHandler(event, filename);
-});
+// const fswatcher2 = fs.watch(paths.webpackPath, { recursive: true }, (event, filename) => {
+//   delete require.cache[require.resolve('../webpack/webpack.config.dev')];
+//   delete require.cache[require.resolve('../webpack/webpack.config.base')];
+//   configfileWatchHandler(event, filename);
+// });
 
-const closeprocess = () => {
-  fswatcher2.close();
-  process.exit();
-};
+// const closeprocess = () => {
+//   fswatcher2.close();
+//   process.exit();
+// };
 
-process.on('SIGINT', closeprocess);
-process.on('uncaughtException', closeprocess);
-process.on('unhandledRejection', closeprocess);
+// process.on('SIGINT', closeprocess);
+// process.on('uncaughtException', closeprocess);
+// process.on('unhandledRejection', closeprocess);
---
+++ vite/paths.js
@@ -0,0 +1,22 @@
+// const path = require('path-browserify');
+
+// import path from 'path-browserify';
+
+// const directorypath = process.cwd();
+
+// const reslovePath = (relativePath) => path.join(directorypath, relativePath);
+
+// export default {
+//   // project directory path
+//   directoryPath: reslovePath('.'),
+//   // package.json path
+//   packageJsonPath: reslovePath('package.json'),
+//   // webapack config path
+//   webpackPath: reslovePath('/webpack'),
+//   // script path
+//   scriptPath: reslovePath('/scripts'),
+//   // src path
+//   srcPath: reslovePath('/src')
+// };
+
+export default {};
