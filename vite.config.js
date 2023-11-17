94f302a12e3821e978eb55c4840b171dd5ebfd55
Murphy.xie <murphy.xie@yunzhihui.com>
2023-11-17 16:21:56 +0800

    新依赖,增加兼容性,优化一键部署

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 import lagacy from '@vitejs/plugin-legacy';
 import browserslist from 'browserslist';
 
-// console.log(' browserslist ', browserslist.defaults);
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
