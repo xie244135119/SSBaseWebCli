fc1689d05cf1aa0ccf42b5b86de6188c9fde1283
murphy.xie <murphy.xie@cloudwise.com>
2024-07-22 14:53:33 +0800

    开启校验登录开关

--- public/env.config.js
+++ public/env.config.js
@@ -2,7 +2,7 @@ window.ENV = (() => ({
   // runtime console
   console: true,
   // 是否需要登录 <false：忽略登录>
-  checkToken: false,
+  checkToken: true,
   // 网络请求前缀 <两种业务场景 1，统一网关场景统一前缀  2，前端项目部署二级目录>
   requestBaseUrl: ''
   //
