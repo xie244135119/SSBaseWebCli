c990488d27e110d064d4e29af90574a79d065c7e
murphy.xie <murphy.xie@cloudwise.com>
2024-08-16 16:45:09 +0800

    update xlsx

--- src/js/xlsl.ts
+++ src/js/xlsl.ts
@@ -1,6 +1,13 @@
 import * as XLSX from 'xlsx';
 import request from 'axios';
 
+interface ExportTableColumns {
+  title: string;
+  dataIndex?: string | string[];
+  children?: ExportTableColumns[];
+  [key: string]: any;
+}
+
 /**
  * 获取excel数据信息
  * @returns
@@ -12,8 +19,7 @@ const importExcelData = (
   new Promise((reslove) => {
     const webbook = XLSX.read(data, {
       type: 'array',
-      sheets: aWorkSheets.length === 0 ? null : aWorkSheets,
-      cellDates: true
+      sheets: aWorkSheets.length === 0 ? null : aWorkSheets
     });
     let workSheets = aWorkSheets;
     if (workSheets.length === 0) {
@@ -46,47 +52,11 @@ const importExcel = (
 };
 
 /**
- * 将json数据 保存为 excel文件，默认格式为 { test: [{a: 112}] }
- * @param {{string: any[]}} obj 数据
- */
-const sheetsToBlob = (obj: { [key: string]: any[] }): Blob => {
-  // 生成数据
-  const createData = (s) => {
-    const buffer = new ArrayBuffer(s.length);
-    const view = new Uint8Array(buffer);
-    for (let index = 0; index < s.length; index += 1) {
-      // eslint-disable-next-line no-bitwise
-      view[index] = s.charCodeAt(index) & 0xff;
-    }
-    return buffer;
-  };
-  const sheetKeys = Object.keys(obj);
-  const Sheets = {};
-  sheetKeys.forEach((key) => {
-    const sheet = XLSX.utils.json_to_sheet(obj[key]);
-    Sheets[key] = sheet;
-  });
-
-  const workbook = {
-    SheetNames: sheetKeys,
-    Sheets
-  };
-  const wbout = XLSX.write(workbook, {
-    bookType: 'xlsx',
-    bookSST: false,
-    type: 'binary'
-  });
-  const data = createData(wbout);
-  const blob = new Blob([data], { type: 'application/octet-stream' });
-  return blob;
-};
-
-/**
  * 下载文件
  * @param url 下载的地址
  * @param savename 保存的文件名
  */
-function download(url: string | Blob, fileName?: string) {
+function download(url: string | Blob | ArrayBuffer, fileName?: string) {
   let newUrl: string;
   if (typeof url === 'object' && url instanceof Blob) {
     newUrl = URL.createObjectURL(url);
@@ -109,13 +79,147 @@ function download(url: string | Blob, fileName?: string) {
  * @param sheets 数据
  * @param aFileName 文件名称
  */
-function exportExcel(sheets: { [key: string]: any }, fileName: string) {
-  try {
-    const blob = sheetsToBlob(sheets);
-    download(blob, fileName);
-  } catch (error) {
-    console.error(' 文件导出数据 ', error);
+function exportExcel(sheets: { [key: string]: any[] }, fileName: string) {
+  const sheetKeys = Object.keys(sheets);
+  const Sheets = {};
+  sheetKeys.forEach((key) => {
+    const sheet = XLSX.utils.json_to_sheet(sheets[key]);
+    Sheets[key] = sheet;
+  });
+  XLSX.writeFileXLSX(
+    {
+      SheetNames: sheetKeys,
+      Sheets
+    },
+    fileName
+  );
+}
+
+/**
+ * 将数据导出为excel文件
+ * @param sheets 数据
+ * @param aFileName 文件名称
+ */
+function exportExcelFromTable(tablee: HTMLTableElement, fileName?: string) {
+  const workbook = XLSX.utils.table_to_book(tablee);
+  // Process Data (add a new row)
+  // var ws = workbook.Sheets['Sheet1'];
+  // XLSX.utils.sheet_add_aoa(ws, [['Created ' + new Date().toISOString()]], { origin: -1 });
+  XLSX.writeFileXLSX(workbook, fileName);
+}
+
+/**
+ * 根据表格
+ * @param columns 表格列
+ * @param dataList 表格数据
+ * @param fileName 文件名称
+ */
+function exportExcelFromColumns(columns: ExportTableColumns[], dataList: any[], fileName: string) {
+  // 将头部标题数据 拉平
+  const mergeOptions = [];
+  let maxHeaderLevel = 1;
+  const flatteHeaders = (
+    columns: ExportTableColumns[],
+    prevHeaders: ExportTableColumns[] = [],
+    level = 1
+  ) => {
+    const list = [];
+    for (let index = 0; index < columns.length; index++) {
+      const element = columns[index];
+
+      if (element.children?.length > 0) {
+        const resault = flatteHeaders(element.children, [...prevHeaders, element], level + 1);
+        list.push(...resault);
+        // 只处理横向合并问题
+        mergeOptions.push({
+          s: {
+            r: level - 1,
+            c: list.length - resault.length
+          },
+          e: {
+            r: level - 1,
+            c: list.length - 1
+          }
+        });
+      } else {
+        list.push({
+          ...element,
+          prevHeaders: [...prevHeaders, element]
+        });
+      }
+    }
+    maxHeaderLevel = Math.max(maxHeaderLevel, level);
+    return list;
+  };
+
+  const flatteColumns = flatteHeaders(columns, []);
+  const headerDatas: string[][] = [];
+  const dataIndexKeys = [];
+  for (let index = 0; index < flatteColumns.length; index++) {
+    const element = flatteColumns[index];
+    dataIndexKeys.push(element.dataIndex);
+
+    for (let j = 0; j < maxHeaderLevel; j++) {
+      if (!headerDatas[j]) {
+        headerDatas[j] = [];
+      }
+      const headerData = headerDatas[j];
+      const targetElement = element.prevHeaders[j] || {};
+      headerData.push(targetElement.title || '');
+
+      if (j === element.prevHeaders.length - 1) {
+        mergeOptions.push({
+          s: {
+            r: j,
+            c: index
+          },
+          e: {
+            r: maxHeaderLevel - 1,
+            c: index
+          }
+        });
+      }
+    }
   }
+
+  // 从对象中获取值
+  const getValueFromProps = (keys: string[], obj: { [key: string]: any }) => {
+    if (keys.length <= 1) {
+      return obj?.[keys[0]];
+    }
+    const target = obj?.[keys[0]];
+    keys.shift();
+    return getValueFromProps(keys, target);
+  };
+
+  const contentDatas = [];
+  for (let index = 0; index < dataList.length; index++) {
+    const element = dataList[index];
+    const values = [];
+    for (let j = 0; j < dataIndexKeys.length; j++) {
+      const dataIndex = dataIndexKeys[j];
+      let value = null;
+
+      // 添加 data数据
+      if (dataIndex instanceof Array) {
+        value = getValueFromProps([...dataIndex], element);
+      } else {
+        value = getValueFromProps([dataIndex], element);
+      }
+      values.push(value);
+    }
+    contentDatas.push(values);
+  }
+
+  // 创建工作簿
+  const wb = XLSX.utils.book_new();
+  // 转换数据为工作表
+  const contentws = XLSX.utils.aoa_to_sheet([...headerDatas, ...contentDatas]);
+  // 设置单元格合并
+  contentws['!merges'] = mergeOptions;
+  // 添加工作表到工作簿
+  XLSX.utils.book_append_sheet(wb, contentws);
+  XLSX.writeFile(wb, fileName);
 }
 
 /**
@@ -123,7 +227,7 @@ function exportExcel(sheets: { [key: string]: any }, fileName: string) {
  * @param aList 原始数据源
  * @param aUniqePropNames 一组唯一值 属性名称
  */
-const createMultiMap = (aList: any[], aUniqePropNames: string[]): { [key: string]: any } => {
+const createMultiMap = (aList = [], aUniqePropNames: string[]): { [key: string]: any } => {
   const resault = {};
   for (let index = 0; index < aList.length; index += 1) {
     const element = aList[index];
@@ -142,6 +246,21 @@ const createMultiMap = (aList: any[], aUniqePropNames: string[]): { [key: string
   return resault;
 };
 
+/**
+ * 导出json 文件
+ * @param {*} aJsonObject json结构数据
+ * @param {*} aFileName 文件名称
+ */
+const exportJson = (aJsonObject: any, aFileName: string) => {
+  try {
+    const jsonData = JSON.stringify(aJsonObject);
+    const blob = new Blob([jsonData], { type: 'text/json' });
+    download(blob, aFileName);
+  } catch (error) {
+    //   console.log(' 文件导出数据 ', error);
+  }
+};
+
 export default {
   /**
    * 导入excel文件 解析
@@ -155,6 +274,18 @@ export default {
    * 导出excel文件
    */
   exportExcel,
+  /**
+   * 根据表格元素导出excel文件
+   */
+  exportExcelFromTable,
+  /**
+   * @description 根据表格列配置表格数据
+   */
+  exportExcelFromColumns,
+  /**
+   * 导出json
+   */
+  exportJson,
   /**
    * 根据数据本地数据创建映射关系
    */
