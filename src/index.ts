a351ccfa9dc78dcd88436475d71b44ac2b9606d8
murphy.xie <murphy.xie@cloudwise.com>
2024-08-16 09:33:03 +0800

    adjust log

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import './index.module.css';
 RouteIndex.renderDom();
 
 console.log(
-  `%c 系统 %c 版本：${__APP_VERSION__} `,
+  `%c 系统版本 %c ${__APP_VERSION__} `,
   'background: #35495e; padding: 4px; border-radius: 3px 0 0 3px; color: #fff',
   'background: #41b883; padding: 4px; border-radius: 0 3px 3px 0; color: #fff'
 );
